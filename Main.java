62892d0464b564aab0f8cdf16e785f0ec399acc8
arthur_pedroni <161646746+arthurpedroni@users.noreply.github.com>
2025-12-15 18:29:18 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main
+{
+
+
+	public static int[] ler(int[] v) {
+		Scanner s = new Scanner(System.in);
+		for(int x = 0; x < v.length; x++) {
+			v[x] = s.nextInt();
+		}
+		return v;
+	}
+
+	public static int ex1(int[] v) {
+		int mais = 0;
+		int ele = 0;
+		for(int x = 0; x < 10; x++) {
+			int cont = 0;
+			for(int i = 0; i < 10; i++) {
+				if(v[x] == v[i]) {
+					cont++;
+				}
+			}
+			if(mais < cont) {
+				ele = v[x];
+				mais = cont;
+			}
+		}
+		return ele;
+	}
+
+	public static void main(String[] args) {
+		int[] v = new int[10];
+		int opcao = 1;
+		switch (opcao) {
+		case 1:
+			System.out.println(1);
+		}
+
+		v = ler(v);
+		int ele = ex1(v);
+		System.out.println("O que mais aparece C) " + ele);
+	}
+}
