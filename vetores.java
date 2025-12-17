61469977a97179ffbe9b9360c993b63b94efff38
arthurpedroni <arthur.pedroni2@gmail.com>
2025-12-16 22:07:07 -0300

    commit

--- vetores.java
+++ vetores.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 public class vetores
 {
 
-	public static int[] ler(int[] v) {
+	public static int[] lerint(int[] v) {
 		Scanner s = new Scanner(System.in);
 		for(int x = 0; x < v.length; x++) {
 			v[x] = s.nextInt();
+		}
+		return v;
+	}
+
+	public static double[] lerdouble(double[] v) {
+		Scanner s = new Scanner(System.in);
+		for(int x = 0; x < v.length; x++) {
+			v[x] = s.nextDouble();
 		}
 		return v;
 	}
@@ -45,13 +53,36 @@ public class vetores
 		}
 		return resultado;	
 	}
+
+	public static int qtdMinima(double[] v) {
+		double sum = 0;
+		double soma = 0;
+		for(int x = 0;x < v.length;x++){
+			sum = sum + v[x];
+			for(int i = x + 1;i < v.length;i++){
+				if(v[i] > v[x]){
+					double aux = v[i];
+					v[i] = v[x];
+					v[x] = aux;
+				}
+			}
+		}
+		int x;
+		for(x = 0;x < v.length;x++){
+			soma = soma + v[x];
+			if(soma >= (sum / 2)){
+				break;
+			}
+		}
+		return x + 1;
+	}
 	public static void main(String[] args) {
-		int opcao = 2;
+		int opcao = 3;
 		switch (opcao) {
 			case 1:{
 				System.out.println("Elemento que mais aparece no vetor");
 				int[] v = new int[10];
-				v = ler(v);
+				v = lerint(v);
 				int resultado = maiorqtd(v);
 				System.out.println("O que mais aparece é " + resultado);
 				break;
@@ -62,6 +93,14 @@ public class vetores
 				for(int x = 0;resultado[x] != 0;x++){
 					System.out.println("Números não digitados: " + resultado[x]);
 				}
+				break;
+			}
+			case 3:{
+				System.out.println("Quantidade minima de elementos para dar a metade da soma de todos os elementos do array");
+				double[] v = new double[20];
+				v = lerdouble(v);
+				int resultado = qtdMinima(v);
+				System.out.println("São necessários "+ resultado +" elementos para dar a metade da soma");
 				break;
 			}
 		}
